7c4ff9c1ba99c5f205d5478d90d4d1076b2e9824
bayezidhasan14 <137198942+bayezidhasan14@users.noreply.github.com>
2024-02-23 23:01:57 +0000

    #

---
+++ learn-script/variable.js
@@ -0,0 +1,48 @@
+// const accountId = "14444444"
+// let accountEmail = "bayezid@hotmail.com"
+
+// var accountPassword = "12345"
+//  accountCity = "Dhaka"
+
+
+// //   accountId = "14444444" 
+// accountEmail = "xyz.com"
+// accountPassword = "xyzcom"
+// accountCity = "noahdaskjl"
+
+// console.table([accountId,accountEmail,accountPassword,accountCity])
+
+// alert = (3+3);
+// console.log(alert);
+
+let fuck = "69"
+
+let valueInNumber = 88
+let valueInNumber1 = String(valueInNumber)
+
+// console.log(valueInNumber);
+// console.log(typeof (valueInNumber));
+// console.log(typeof (valueInNumber1));
+// console.log(typeof (fuck));
+
+
+// notes
+// "33"=>33    //string to number
+// "33abc"=> NaN  //not a number
+// true => 1; false => 0
+// notes
+
+let  age = 19
+
+
+if ( age >= 18) {
+    console.log("welcome to xnxx");
+} else if (age <= 18) {
+    console.log("drink milk");
+}
+
+
+let isfucking = undefined
+
+let booleanIsLoggedIn = Boolean(isfucking)
+console.log(booleanIsLoggedIn);
